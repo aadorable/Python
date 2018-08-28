99630ce556c7f9ba91b746d05717cd3cbe27801e
aadorable <1615691183@qq.com>
2018-08-28 23:52:47 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import urllib2
+import re
+import json
+from bs4 import BeautifulSoup
+#import MySQLdb
+#yum install python-devel
+#pip install mysql-python
