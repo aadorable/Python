a7832b3b0a3947a560352924f240658f44c825ba
aadorable <1615691183@qq.com>
2018-08-30 23:42:19 +0800

    add ParseMainPage

--- test.py
+++ test.py
@@ -22,6 +22,21 @@ def Test1():
 def Test2():
     print OpenPage("http://jy.51uns.com:8022/Frame/Data/jdp.ashx?rnd=1528794488557&fn=GetZhaopinList&StartDate=2000-01-01&SearchKey=&InfoType=-1&CompanyAttr=&CompanyType=&Area=&City=&CompanyProvice=&Post=&Zhuanye=&XLkey=&Age=&start=0&limit=15&DateType=999&InfoState=1&WorkType=0&CompanyKey=")
 
+#解析主页内容，获取招聘信息的url
+def ParseMainPage(page):
+    #json.dumps() 转化数据成json格式
+    #json.loads() 转化json格式成python数据类型
+    data = json.loads(page)
+    #解析后的Data是一个大的字典,rows是招聘信息数据的key
+    #rows里面保存了一个包含多个招聘信息的list
+    #list里面的每个元素都是一条招聘信息，保存为字典类型
+    rows = data["rows"]
+    prefix = "http://jy.51uns.com:8022/Frame/Data/jdp.ashx?rnd=1533005391497&fn=GetOneZhaopin&StartDate=2000-01-01&JobId="
+    IdList = []
+    for item in rows:
+        IdList.append(prefix + item['Id'])
+    #return [prefix + item["Id"] for item in rows]
+    return IdList
 
 #mysql -u root -p < table.sql
 if __name__ == "__main__":
